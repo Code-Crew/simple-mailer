76aa5167c7d09cd026ccfb575ab490a011f3e86d
Jack <jack@code-crew.net>
2013-11-29 20:50:07 -0500

    a few tweaks

--- webform.php
+++ webform.php
@@ -1,5 +1,7 @@
 <?php 
 
+/* Configure */
+
 $option_redirect = "http://www.code-crew.net";
 $option_email_target = "test@domain.com";
 
@@ -8,7 +10,16 @@ $field_name = "name";
 $field_phone = "phone";
 $field_message = "message";
 
-mail($option_email_target, "WEBFORM", $_POST[$field_message]);
+/* Main Code */
+
+$message =	"{$_POST[$field_name]}\n{$_POST[$field_phone]}\n\n" .
+			"{$_POST[$field_message]}\n"
+
+$headers =	"From: {$_POST[$field_email]}\r\n" .
+			"Reply-To: {$_POST[$field_email]}\r\n" .
+			"X-Mailer: PHP/" . phpversion();
+
+mail($option_email_target, $_POST[$field_name], $_POST[$field_message], $headers);
 
 if (strlen ($option_redirect) > 10) { header("Location: ".$option_redirect); }
 else { header("Location: /"); }
